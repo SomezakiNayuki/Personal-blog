ac3803a84ac6528a4842b439590d2be8537a1aee
Henry <sunhaoyu468@gmail.com>
2022-01-17 01:16:01 +1100

    thymeleaf added to page prototype

--- blog-backend/src/main/java/com/somezaki/blogbackend/web/indexController.java
+++ blog-backend/src/main/java/com/somezaki/blogbackend/web/indexController.java
@@ -4,14 +4,26 @@ import com.somezaki.blogbackend.exception.NotFoundException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class indexController {
 
+    /*
+     * @GetMapping("/{id}/{name}")
+     * public String index(@PathVariable Integer id, @PathVariable String name) {
+     * return "index";
+     * }
+     */
+
     @GetMapping("/")
-    public String index() {
-        // int i = 9 / 0;
-        throw new NotFoundException("blog do not exist");
+    public String index(Integer id, String name) {
+        return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(Integer id, String name) {
+        return "blog";
     }
 
 }
